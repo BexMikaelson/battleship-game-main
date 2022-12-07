8d4b837ea3a62ed65a67e1780e7dd855ba2c3b40
Heidi sjöberg <heidi.alexx8@gmail.com>
2022-12-07 19:10:24 +0100

    checks and display hit, miss, sunk ship

--- battleship-game/src/assets/TheGame/ComputerBoard.jsx
+++ battleship-game/src/assets/TheGame/ComputerBoard.jsx
@@ -39,6 +39,70 @@ export const ComputerBoard = ({
     compLayout
   );
 
+   // Check what's at the square and decide what next
+   const fireTorpedo = (index) => {
+    if (compLayout[index] === 'ship') {
+      const newHits = [
+        ...hitsByPlayer,
+        {
+          position: indexToCoords(index),
+          type: SQUARE_STATE.hit,
+        },
+      ];
+      setHitsByPlayer(newHits);
+      return newHits;
+    }
+    if (compLayout[index] === 'empty') {
+      const newHits = [
+        ...hitsByPlayer,
+        {
+          position: indexToCoords(index),
+          type: SQUARE_STATE.miss,
+        },
+      ];
+      setHitsByPlayer(newHits);
+      return newHits;
+    }
+  };
+
+  const playerTurn = gameState === 'player-turn';
+  const playerCanFire = playerTurn && !checkIfGameOver();
+
+  let alreadyHit = (index) =>
+    compLayout[index] === 'hit' ||
+    compLayout[index] === 'miss' ||
+    compLayout[index] === 'ship-sunk';
+
+  let compSquares = compLayout.map((square, index) => {
+    return (
+      <div
+        // Only display hit, miss, or sunk ship
+        className={
+          stateToClass[square] === 'hit' ||
+          stateToClass[square] === 'miss' ||
+          stateToClass[square] === 'ship-sunk'
+            ? `square ${stateToClass[square]}`
+            : `square`
+        }
+        key={`comp-square-${index}`}
+        id={`comp-square-${index}`}
+        onClick={() => {
+          if (playerCanFire && !alreadyHit(index)) {
+            const newHits = fireTorpedo(index);
+            const shipsWithSunkFlag = updateSunkShips(newHits, computerShips);
+            const sunkShipsAfter = shipsWithSunkFlag.filter((ship) => ship.sunk).length;
+            const sunkShipsBefore = computerShips.filter((ship) => ship.sunk).length;
+            if (sunkShipsAfter > sunkShipsBefore) {
+              ('sunk');
+            }
+            setComputerShips(shipsWithSunkFlag);
+            handleComputerTurn();
+          }
+        }}
+      />
+    );
+  });
+
  
 
 
