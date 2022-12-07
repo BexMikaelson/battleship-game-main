87fda8901cdaa056782ffde7e596082103e680ee
Heidi sjöberg <heidi.alexx8@gmail.com>
2022-12-07 13:57:07 +0100

    ships on empty layout

--- battleship-game/src/assets/TheGame/PlayerBoard.jsx
+++ battleship-game/src/assets/TheGame/PlayerBoard.jsx
@@ -9,3 +9,19 @@ import {
   canBePlaced,
 } from '../Helpers/Layout';
 
+export const PlayerBoard = ({
+    currentlyPlacing,
+    setCurrentlyPlacing,
+    rotateShip,
+    placeShip,
+    placedShips,
+    hitsByComputer,
+  }) => {
+    // Player ships on empty layout
+    let layout = placedShips.reduce(
+      (prevLayout, currentShip) =>
+        putEntityInLayout(prevLayout, currentShip, SQUARE_STATE.ship),
+      generateEmptyLayout()
+    );
+  
+   
