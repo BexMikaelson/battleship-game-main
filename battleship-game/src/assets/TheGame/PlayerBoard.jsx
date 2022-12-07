cb5300fb6fa61017413b3bcbd165235b35e86638
Heidi sjöberg <heidi.alexx8@gmail.com>
2022-12-07 13:56:02 +0100

    creating player board

---
+++ battleship-game/src/assets/TheGame/PlayerBoard.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {
+  SQUARE_STATE,
+  stateToClass,
+  generateEmptyLayout,
+  putEntityInLayout,
+  indexToCoords,
+  calculateOverhang,
+  canBePlaced,
+} from '../Helpers/Layout';
+
